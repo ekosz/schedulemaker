d7bb8ebae97b6f2074fcca434ae38a17908bc251
Eric Koslow <ekoslow@gmail.com>
2011-10-21 18:26:10 -0400

    Converted index, header, footer sections to twitter bootcamp.

--- index.php
+++ index.php
@@ -18,36 +18,31 @@ if(isset($_GET['s'])) {
 
 require "./inc/header.inc";
 ?>
-<h1>Schedule Maker</h1>
-<table id="mainMenu">
-	<tr>
-		<td><!-- image goes here --></td>
-		<td><!-- image goes here --></td>
-		<td><!-- image goes here --></td>
-	</tr>
-	<tr>
-		<td><a href='generate.php'>Make a Schedule</a></td>
-		<td><a href='browse.php'>Browse Courses</a></td>
-		<td><a href='roulette.php'>Course Roulette</a></td>
-	</tr>
-</table>
+<div class="hero-unit" style="margin-top: 3em;">
+  <h1>Schedule Maker</h1>
+  <div class="row" style="padding-top: 2em;">
+    <div class="span3"><a class="btn primary" href='generate.php'>Make a Schedule</a></div>
+    <div class="span3"><a class="btn primary" href='browse.php'>Browse Courses</a></div>
+    <div class="span3"><a class="btn primary" href='roulette.php'>Course Roulette</a></div>
+  </div>
+</div>
 <h2>Project Progress</h2>
 <ul>
-	<li>Index Page/Styling - <span class='b'>Partial</span></li>
-	<li>Schedule Form - <span class='c'>COMPLETE (08-12-11)</span></li>
-	<li>Schedule Generator - <span class='c'>COMPLETE (09-01-11)</span></li>
-	<li>Course Roulette - <span class='c'>COMPLETE (08-06-11)</span></li>
+	<li>Index Page/Styling - <span class='label warning'>Partial</span></li>
+	<li>Schedule Form - <span class='label success'>COMPLETE (08-12-11)</span></li>
+	<li>Schedule Generator - <span class='label success'>COMPLETE (09-01-11)</span></li>
+	<li>Course Roulette - <span class='label success'>COMPLETE (08-06-11)</span></li>
 	<li>Cronjob Status - Not complete</li>
-	<li>AJAX Integration - <span class='c'>COMPLETE</span></li>
-	<li>Courses DB - <span class='c'>COMPLETE (08-03-11)</span></li>
-	<li>Saved Schedule Lookup - <span class='c'>COMPLETE (10-07-11)</span></li>
-	<li>Saved Schedule Cleaner - <span class='c'>COMPLETE (10-07-11)</span></li>
-	<li>Schedule Output - <span class='c'>COMPLETE (09-17-11)</span></li>
-	<li>Social Media Sharing - <span class='c'>COMPLETE (09-17-11)</span></li>
-	<li>iCal Exporting - <span class='b'>Partial</span></li>
-	<li>Browse Courses - <span class='c'>COMPLETE (10-08-11)</span></li>
-	<li>Scraper Migration - <span class='c'>COMPLETE (10-13-11)</li>
-	<li>Migrator: Courses - <span class='c'>COMPLETE (08-03-11)</span></li>
-	<li>Migrator: Saves Schedules - <span class='c'>COMPLETE (08-04-11)</span></li>
+	<li>AJAX Integration - <span class='label success'>COMPLETE</span></li>
+	<li>Courses DB - <span class='label success'>COMPLETE (08-03-11)</span></li>
+	<li>Saved Schedule Lookup - <span class='label success'>COMPLETE (10-07-11)</span></li>
+	<li>Saved Schedule Cleaner - <span class='label success'>COMPLETE (10-07-11)</span></li>
+	<li>Schedule Output - <span class='label success'>COMPLETE (09-17-11)</span></li>
+	<li>Social Media Sharing - <span class='label success'>COMPLETE (09-17-11)</span></li>
+	<li>iCal Exporting - <span class='label warning'>Partial</span></li>
+	<li>Browse Courses - <span class='label success'>COMPLETE (10-08-11)</span></li>
+	<li>Scraper Migration - <span class='label success'>COMPLETE (10-13-11)</li>
+	<li>Migrator: Courses - <span class='label success'>COMPLETE (08-03-11)</span></li>
+	<li>Migrator: Saves Schedules - <span class='label success'>COMPLETE (08-04-11)</span></li>
 </ul>
 <? require "./inc/footer.inc"; ?>
